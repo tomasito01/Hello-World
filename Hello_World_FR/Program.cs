60c4193e91d78f76a40fc0af87e39e53eceb7e94
tomasito01 <58252224+tomasito01@users.noreply.github.com>
2020-03-25 23:32:32 +0100

    Add files via upload

---
+++ Hello_World_FR/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_World_FR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! (Farkas Róbet Tamás)");
+        }
+    }
+}
